0d7440ef75928db04f66a40bf7e8996c79b35aab
VCoklat <VCoklat@users.noreply.github.com>
2017-09-17 00:44:27 +0700

    initial_commit

---
+++ RISE-Multipurpose html template/template-assets/renungan.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Renungan Kuat dan Menang</title>
+</head>
+<body>
+<center>
+<strong><h3>Kuat dan Menang</h3></strong>
+<img alt="1" src="D:\me_iamgod.PNG" />
+<p> Link : <a href="http://sabdaweb.sabda.org/passages/?p=ula+33:1-11">Ulangan 33:1-11</a></p>
+<p align="justify" style="margin-left: 5%;margin-right: 5%">
+												"Nak.. apapun yang terjadi, Tuhan itu sayang sama kamu, terlebih lagi Tuhan masih memberikan kesempatan untuk kamu hidup.."
+Ada kalanya kita berpikir, “Mengapa hal ini Tuhan ijinkan terjadi dalam hidupku.” yang mungkin hal itu terasa berat bagi kita. Saat Musa sudah lanjut umurnya, Tuhan memerintahkan Musa agar ia menyampaikan berkat kepada bangsa Israel. Tidak hanya memberikan berkat tetapi nubuatan Tuhan dan bagaimana mereka harus hidup di dalam kehendak dan recana Tuhan. Ada empat hal bagaimana kita kuat dan menang dalam menghadapi permasalahan yang Tuhan ijikan. 
+
+Musa berkata, “Sungguh Ia mengasihi umatNya; semua orangNya yang kudus; di dalam tanganMu-lah mereka, pada kakiMu-lah mereka duduk, menangkap sesuatu dari firmanMu, ..suatu milik bagi jemaah Yakub. Ia MENJADI RAJA di Yesyurun (Israel), ketika kepala-kepala bangsa datang berkumpul, yakni segala suku Israel bersama-sama.” (ay 3-5). Hal yang pertama, sadarilah Tuhanlah yang mempunyai tujuan dalam hidup kita. Seperti yang Paulus katakan, “Namun aku hidup, tetapi bukan lagi aku sendiri yang hidup, melainkan Kristus yang hidup di dalam aku. Dan hidupku yang kuhidupi sekarang di dalam daging, adalah hidup oleh iman dalam Anak Allah yang telah mengasihi aku dan menyerahkan diriNya untuk aku. (Galatia 2:20). 
+
+Selanjutnya dikatakan, “Biarlah Ruben HIDUP DAN JANGAN MATI, tetapi biarlah orang2nya sedikit jumlahnya." (ay 6). Hal yang kedua adalah permasalahan bukanlah hal yang utama. Delapan puluh persen hidup kita ditentukan oleh bagaimana respon kita, buah yang kita hasilkan. “Karena bagiku hidup adalah Kristus dan mati adalah keuntungan. Tetapi jika aku harus hidup di dunia ini, itu berarti bagiku bekerja memberi buah.” (Filipi 1:21-22). 
+
+Berkat selanjutnya kepada suku Yehuda, katanya: "Dengarlah, ya TUHAN, suara Yehuda dan bawalah dia KEPADA BANGSANYA. Berjuanglah baginya dengan tanganMu, dan jadilah Engkau penolongnya melawan musuhnya." (ay 7). Hal yang ketiga adalah Tuhan sedang memurnikan kita, Tuhan sedang menyiapkan kita anak-anakNya menjadi alatNya yang luar biasa, dan Tuhan juga mempersiapkan kita menjadi mempelai-mempelaiNya yang tetap memiliki pelita (Matius 25). 
+
+Kemuadian tentang Lewi Musa berkata: "Biarlah Tumim dan Urim-Mu (batu yang digunakan untuk menentukan keputusan Allah) menjadi kepunyaan orang yang Kau kasihi, yang telah Kau coba di Masa, dengan siapa Engkau berbantah dekat mata air Meriba.. Sebab orang-orang Lewi itu BERPEGANG pada firmanMu dan MENJAGA perjanjianMu; mereka mengajarkan peraturan-peraturanMu kepada Yakub, hukumMu kepada Israel; mereka menaruh ukupan wangi-wangian di depanMu dan korban yang terbakar seluruhnya di atas mezbahMu" (Ay 4-10). Tidak ragu dan tetap teguh akan janji Tuhan, meskipun sekeliling kita telah tawar hati. Abraham yang taat saat Tuhan menginginkan Ishak. Daniel yang tetap hanya menyembah Tuhan satu-satunya Allah meskipun dihadapkan kepada maut. 
+
+Karena itu marilah kita tetap berjalan teguh dalam setiap hal yang kita hadapi di dalam firmanNya, karena kita tahu ada kemenangan dan mahkota kemuliaan yang Tuhan sediakan bagi kita semua mempelai-mempelaiNya. 
+
+Sebuah doa untuk hari ini,
+Bapa surgawi, aku bersyukur untuk kasih setiaMu di dalam kehidupanku, Engkau yang membentuk aku, Engkau juga yang akan memahkotai aku. Ajar aku tetap setia kepadaMu, tetap setia pada firmanMu dan memuliakan Engkau dalam setiap ucapan dan perbuatanku. Terima kasih Tuhan, dalam nama Yesus,amin.
+											</p>
+											</center>
+</body>
+</html>
---
+++ RISE-Multipurpose html template/template-assets/renungan2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Renungan Merasa Hebat</title>
+</head>
+<body>
+<center>
+<strong><h3>Merasa Hebat</h3></strong>
+<img alt="2" src="D:\me_janji3.PNG" />
+<p> Link : <a href="http://sabdaweb.sabda.org/passages/?p=ula+32:40-52">Ulangan 32:40-52</a></p>
+											<p align="justify" style="margin-left: 5%;margin-right: 5%">
+"Pa.. temanku curang padaku, besok aku mau balas dia.."
+Saat kita di dalam kesesakan, saat kita merasa dicurangi, apakah tanggapan kita, apakah kita merasa sanggup dan ingin membalasnya? Kesesakan yang sama juga pernah dialami oleh bangsa Israel saat musuh-musuh mengalahkan mereka. Saat kita terdang mendapati masalah yang tak kunjung selesai ada kalanya kita ingin menyudahi dengan cara kita. Tetapi inilah tiga janji Tuhan kepada bangsa Israel dan khususnya untuk kita saat ini. 
+
+Allah berfirman, "Sesungguhnya, Aku MENGANGKAT TANGANKU ke langit, dan berfirman: Demi Aku yang hidup selama-lamanya.” (ayat 40). Janji Allah yang pertama kepada kita adalah Tuhan bukan Allah yang diam. Karena itu jangan pernah curiga, jangan pernah menyerah, Tuhan tau kekuatan kita, dan Dia akan bertindak di waktu yang tepat. “Karena mata TUHAN menjelajah seluruh bumi untuk melimpahkan kekuatanNya kepada mereka yang bersungguh hati terhadap Dia.” (2Tawarikh 16:9). 
+
+Selanjutnya dikatakan, “..apabila Aku mengasah pedangKu yang berkilat-kilat, dan TANGANKU MEMEGANG PENGHUKUMAN, maka Aku membalas dendam kepada lawanKu, dan mengadakan pembalasan kepada yang membenci Aku.” (ayat 41). Janji Tuhan yang kedua kepada kita adalah bahwa Tuhan yang empunya peraturan tertinggi, semua hukum yang ada akan tunduk dan diadili menurut hukum Tuhan. Oleh karena itu tetaplah berada di dalam firmanNya. Yesus berkata, “Aku tidak dapat berbuat apa-apa dari diriKu sendiri; Aku menghakimi sesuai dengan apa yang Aku dengar, dan penghakimanKu adil, sebab Aku tidak menuruti kehendakKu sendiri, melainkan kehendak Dia yang mengutus Aku.” (Yohanes 5:30). 
+
+Kemudian berkatalah Allah, ”Aku akan memabukkan anak panahKu dengan darah, dan pedangKu akan memakan daging: darah orang-orang yang mati tertikam dan orang-orang yang tertawan, DARI KEPALA-KEPALA MUSUH yang berambut panjang. BERSORAK-SORAILAH, hai bangsa-bangsa karena umatNya, sebab Ia membalaskan darah hamba-hambaNya, Ia membalas dendam kepada lawanNya, dan mengadakan PENDAMAIAN bagi tanah umatNya." (Ay 42-43). Janji Allah yang ketiga adalah Tuhan pasti memberi kita kemenangan dan damai sejahtera. Allah telah membuktikan kasihNya melalui Yesus. Yesus berkata, “Gembala yang baik memberikan nyawanya bagi domba-dombanya, ..domba-domba itu harus Kutuntun juga dan mereka akan mendengarkan suaraKu dan mereka akan menjadi satu kawanan dengan satu gembala.” (Yohanes 10:11,16). 
+
+Maka Berkatalah Musa, "Perhatikanlah segala perkataan ini, supaya kamu memerintahkannya kepada anak-anakmu untuk melakukan dengan setia. Sebab perkataan ini bukanlah perkataan hampa bagimu, tetapi itulah hidupmu, dan dengan perkataan ini akan lanjut umurmu di tanah, ke mana kamu pergi.. untuk mendudukinya." (ayat 46-47). Paulus berkata, “..jangan ada orang yang membalas kejahatan dengan kejahatan, tetapi usahakanlah senantiasa yang baik.. terhadap semua orang. Bersukacitalah senantiasa. Tetaplah berdoa. Mengucap syukurlah dalam segala hal, sebab itulah yg dikehendaki Allah di dalam Kristus Yesus bagi kamu.” (1Tesalonika 5:15-18). 
+
+Sebuah doa untuk hari ini,
+Bapa surgawi, terima kasih buat kebenaranMu yang memberikan aku pengharapan dan kekuatan baru di dalam hidupku. Aku mau, biarlah di dalam segala keadaan dan setiap permasalahan yang Engkau ijinkan terjadi, aku tetap kuat dan percaya pada janji firmanMu. Hingga waktuMu tiba nanti, aku akan memperoleh kemenangan dan damai sejahtera yang telah Engkau sediakan. Terima kasih Tuhan, dalam nama Yesus, amin. 
+
+											</p></center>
+
+</body>
+</html>
